819a7749c4ee099f5770202fb5741eda6371b883
Daniel Houck <GitHub@DRHouck.me>
2021-12-09 16:53:52 -0600

    Change plans for debug() vs info() vs print() in template

--- day/template/part1.py
+++ day/template/part1.py
@@ -8,10 +8,11 @@ def main(args):
 	line_count = 0
 	lines = []
 	for line in args.input:
+		debug(f'Considering line: {line!r}')
 		line_count += 1
 		lines.append(line)
-	debug(f'Lines: {pformat(lines)}')
-	info(f'Result: 17')
+	info(f'There were {line_count} lines')
+	print(f'Result: 17')
 
 if __name__ == '__main__':
 	parser = aoc.get_argparser()
