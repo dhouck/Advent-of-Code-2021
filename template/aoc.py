9b2b4238450ba18579fa4878cab399fb2db33b06
Daniel Houck <GitHub@DRHouck.me>
2021-12-16 07:39:07 -0600

    Add indent capabilities to logging
    This doesnʼt easily match the standard idea of a log as a searchable
    or filterable sequence of events because the context now depends more
    than usual on previous messages, but it helps when reading the log,
    which is what Iʼm actually doing here.

--- template/aoc.py
+++ template/aoc.py
@@ -12,11 +12,13 @@ try:
 except ImportError:
 	configLogging = logging.basicConfig
 
+
 @functools.wraps(argparse.ArgumentParser)
 def get_argparser(*args, **kwargs):
 	parser = argparse.ArgumentParser(*args, **kwargs)
 	add_default_arguments(parser)
 	return parser
+
 
 def add_default_arguments(argparser):
 	argparser.add_argument('input', type=argparse.FileType('r'), default=sys.stdin)
@@ -26,12 +28,40 @@ def add_default_arguments(argparser):
 	log_group.add_argument('--log-level', choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'],
 	                       default='INFO')
 
+
 def parse_args(parser):
 	args = parser.parse_args()
 	set_up_logging(args)
 	return args
 
+
+class Indenter(logging.Filter):
+	__slots__ = 'level'
+	
+	def __init__(self):
+		super().__init__()
+		self.level = 0
+	
+	def __str__(self):
+		return '│ '*(self.level-1) + ('├╴' if self.level else '')
+	
+	def __enter__(self):
+		self.level += 1
+	
+	def __exit__(self, *_):
+		self.level -= 1
+	
+	def filter(self, record):
+		record.indent = str(self)
+		record.indent_level = self.level
+		return True
+
+
+indent = Indenter()
+
+
 def set_up_logging(args):
 	new_level = logging.getLevelName(args.log_level) - 10*args.verbose + 10*args.quiet
 	new_level = max(min(new_level, logging.CRITICAL + 10), 0)
-	configLogging(style='{', format='{levelname}: {message}', level=new_level)
+	configLogging(style='{', format='{levelname}: {indent}{message}', level=new_level)
+	logging.getLogger().addFilter(indent)
